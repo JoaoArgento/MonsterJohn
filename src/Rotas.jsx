45efdc38e0cb753a7835a7c19c9be02632bc8b19
Joao <Joaoargento12@gmail.com>
2025-02-28 14:24:11 -0300

    troca de hash router

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Routes, Route} from "react-router-dom";
 import React from "react";
 import Vitrine from "./Pages/Vitrine"
 import Produto from "./Pages/Produto"
@@ -7,7 +7,7 @@ import Promocoes from "./Pages/Promocoes";
 
 export default function Rotas()
 {
-    return<BrowserRouter basename="/MonsterJohn">
+    return<HashRouter>
         <Routes>
             <Route index = "/" element = {<Vitrine/>}/>
             <Route path = "/produto/:codigo" element = {<Produto/>}/>
@@ -15,5 +15,5 @@ export default function Rotas()
             <Route path = "/carrinho" element = {<Carrinho/>}/>
             <Route path = "/produto" element = {<Produto/>}/>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 }
